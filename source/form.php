ccd482f8397b228e9b9d788b23af7447ccfb9c8a
DariaAkimova <akimova-dv@mail.ru>
2022-10-04 10:21:36 +0300

    Настройка sql для данных формы

--- source/form.php
+++ source/form.php
@@ -2,6 +2,8 @@
 $reg_ex_phone = '/^[\d\+][\d\(\)\ -]{4,14}\d$/';
 $reg_ex_email = '/^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i';
 
+$mysql= new mysqli('localhost', 'u1790818_default', 'fArU1pC015BI9ZSx', 'u1790818_stitch-flow');
+
 $mail_subject = 'Новая заявка Stitch flow';
 $mail_to = 'stitch-flow@mail.ru';
 
@@ -12,24 +14,35 @@ if (isset($_POST['user-name']) && isset($_POST['phone']) && isset($_POST['email'
     $comment = trim(htmlspecialchars($_POST['comments']));
     $time = date('Y-m-d H:i:s');
 
-    $mail_message = $time . "\n" . 'Name:' . ' ' . $name . "\n" . 'Phone:' . ' ' . $phone . "\n" . 'email:' . ' ' . $email . "\n" . 'comments:' . ' ' . $comments . "\n";
+    $mail_message = $time . "\n" . 'Name:' . ' ' . $name . "\n" . 'Phone:' . ' ' . $phone . "\n" . 'email:' . ' ' . $email . "\n" . 'comments:' . ' ' . $comment . "\n";
+
+    $mysql->query("SET NAMES 'utf8'");
+   $mysql_error = $mysql->connect_error;
+    if ($mysql_error) {
+                $sqlErrorFile = fopen('sql-error.txt', 'a');
+        fwrite($sqlErrorFile, $time. ' ' . $mysql_error . "\n");
+        fclose($sqlerrorFile);
+    } else {
+    $mysql->query("INSERT INTO users (name, email, phone, user_comments) VALUES('$name', '$email', '$phone', '$comment')");
+    }
+    $mysql->close();
 
     mail($mail_to, $mail_subject, $mail_message);
 
     if ((preg_match($reg_ex_phone, $phone)) && (preg_match($reg_ex_email, $email))) {
         $dataFile = fopen('users.txt', 'a');
-        fwrite($dataFile, $time . ' ' . 'Name:' . ' ' . $name . ' ' . 'Phone:' . ' ' . $phone . ' ' . 'email:' . ' ' . $email . ' ' . 'comments:' . ' ' . $comments . "\n");
+        fwrite($dataFile, $time . ' ' . 'Name:' . ' ' . $name . ' ' . 'Phone:' . ' ' . $phone . ' ' . 'email:' . ' ' . $email . ' ' . 'comments:' . ' ' . $comment . "\n");
         fclose($dataFile);
         echo "Спасибо, ваша заявка принята. Вы можете <a href=\"index.html\"> вернуться на сайт </a>."; /* на случай проблем с js*/
     } else {
         $errorFile = fopen('error.txt', 'a');
-        fwrite($errorFile, $time . ' ' . 'Name:' . ' ' . $name . ' ' . 'Phone:' . ' ' . $phone . ' ' . 'email:' . ' ' . $email . "\n");
+        fwrite($errorFile, $time . ' ' . 'Name:' . ' ' . $name . ' ' . 'Phone:' . ' ' . $phone . ' ' . 'email:' . ' ' . $email . ' ' . $comment . "\n");
         fclose($errorFile);
         echo " <a href=\"index.html\">Вернитесь на сайт </a> и введите корректные данные! Таже вы можете написать мне по указанным контактам."; /* на случай проблем с js */
     }
 } else {
     $emptyFile = fopen('no-data.txt', 'a');
-    fwrite($emptyFile, $time . ' ' . 'Name:' . ' ' . $name . ' ' . 'Phone:' . ' ' . $phone . ' ' . 'email:' . ' ' . $email . "\n");
+    fwrite($emptyFile, $time . ' ' . 'Name:' . ' ' . $name . ' ' . 'Phone:' . ' ' . $phone . ' ' . 'email:' . ' ' . $email . ' ' . $comment . "\n");
     fclose($emptyFile);
     echo "<a href=\"index.html\">Вернитесь на сайт </a> и введите данные! Таже вы можете написать мне по указанным контактам."; /* на случай проблем с js*/
 }
